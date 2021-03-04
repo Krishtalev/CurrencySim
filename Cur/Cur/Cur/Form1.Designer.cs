d0c971e58c95a421020cf058abc5ef860e41f0e1
Krishtalev <47716836+Krishtalev@users.noreply.github.com>
2021-03-04 15:13:14 +0700

    Add files via upload

---
+++ Cur/Cur/Cur/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Cur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.edPercent = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelDollars = new System.Windows.Forms.Label();
+            this.labelRubles = new System.Windows.Forms.Label();
+            this.edValue = new System.Windows.Forms.NumericUpDown();
+            this.btStart = new System.Windows.Forms.Button();
+            this.edDays = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edRate = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edPercent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edDays)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.edPercent);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.labelDollars);
+            this.panel1.Controls.Add(this.labelRubles);
+            this.panel1.Controls.Add(this.edValue);
+            this.panel1.Controls.Add(this.btStart);
+            this.panel1.Controls.Add(this.edDays);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.edRate);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(956, 132);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 98);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Процент";
+            // 
+            // edPercent
+            // 
+            this.edPercent.DecimalPlaces = 2;
+            this.edPercent.Location = new System.Drawing.Point(141, 96);
+            this.edPercent.Margin = new System.Windows.Forms.Padding(4);
+            this.edPercent.Name = "edPercent";
+            this.edPercent.Size = new System.Drawing.Size(128, 22);
+            this.edPercent.TabIndex = 13;
+            this.edPercent.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            65536});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(740, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 28);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Купить доллары";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(614, 96);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(172, 28);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Продать все доллары";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(740, 47);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 28);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Продать доллары";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(434, 96);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Купить на все";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelDollars
+            // 
+            this.labelDollars.AutoSize = true;
+            this.labelDollars.Location = new System.Drawing.Point(431, 60);
+            this.labelDollars.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDollars.Name = "labelDollars";
+            this.labelDollars.Size = new System.Drawing.Size(113, 17);
+            this.labelDollars.TabIndex = 7;
+            this.labelDollars.Text = "Мои доллары: 0";
+            // 
+            // labelRubles
+            // 
+            this.labelRubles.AutoSize = true;
+            this.labelRubles.Location = new System.Drawing.Point(431, 25);
+            this.labelRubles.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelRubles.Name = "labelRubles";
+            this.labelRubles.Size = new System.Drawing.Size(118, 17);
+            this.labelRubles.TabIndex = 6;
+            this.labelRubles.Text = "Мои рубли: 1000";
+            // 
+            // edValue
+            // 
+            this.edValue.DecimalPlaces = 2;
+            this.edValue.Location = new System.Drawing.Point(604, 15);
+            this.edValue.Margin = new System.Windows.Forms.Padding(4);
+            this.edValue.Name = "edValue";
+            this.edValue.Size = new System.Drawing.Size(128, 22);
+            this.edValue.TabIndex = 5;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(277, 40);
+            this.btStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(152, 51);
+            this.btStart.TabIndex = 4;
+            this.btStart.Text = "Пропустить дни";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // edDays
+            // 
+            this.edDays.Location = new System.Drawing.Point(141, 55);
+            this.edDays.Margin = new System.Windows.Forms.Padding(4);
+            this.edDays.Name = "edDays";
+            this.edDays.Size = new System.Drawing.Size(128, 22);
+            this.edDays.TabIndex = 3;
+            this.edDays.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Дней";
+            // 
+            // edRate
+            // 
+            this.edRate.DecimalPlaces = 2;
+            this.edRate.Location = new System.Drawing.Point(141, 20);
+            this.edRate.Margin = new System.Windows.Forms.Padding(4);
+            this.edRate.Name = "edRate";
+            this.edRate.Size = new System.Drawing.Size(128, 22);
+            this.edRate.TabIndex = 1;
+            this.edRate.Value = new decimal(new int[] {
+            7433,
+            0,
+            0,
+            131072});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Текущий курс";
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(0, 132);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsValueShownAsLabel = true;
+            series1.LabelFormat = "F2";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(956, 422);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 554);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edPercent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edDays)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.NumericUpDown edDays;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown edRate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label labelRubles;
+        private System.Windows.Forms.NumericUpDown edValue;
+        private System.Windows.Forms.Label labelDollars;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown edPercent;
+    }
+}
+
---
+++ Cur/Cur/Cur/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const double k = 0.05;
+        public Wallet account = new Wallet();
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            
+            double rate = (double)edRate.Value;
+            int days = (int)edDays.Value;
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(0, rate);
+
+            for (int i = 1; i <= days; i++)
+            {
+                rate = rate * (1 + k * (random.NextDouble() - 0.5));
+                edRate.Value = (decimal)rate;
+                chart1.Series[0].Points.AddXY(i, rate);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double rate = (double)edRate.Value;
+            double percent = (double)edPercent.Value;
+            double val = (double)edValue.Value;
+            account.buyDollars(val, rate, percent);
+            labelDollars.Text = "Мои доллары: " + account.myDollars;
+            labelRubles.Text = "Мои рубли: " + account.myRubles;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double rate = (double)edRate.Value;
+            double percent = (double)edPercent.Value;
+            double val = (double)edValue.Value;
+            account.sellDollars(val, rate, percent);
+            labelDollars.Text = "Мои доллары: " + account.myDollars;
+            labelRubles.Text = "Мои рубли: " + account.myRubles; ;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double rate = (double)edRate.Value;
+            double percent = (double)edPercent.Value;
+            account.buyAllDollars(rate, percent);
+            labelDollars.Text = "Мои доллары: " + account.myDollars;
+            labelRubles.Text = "Мои рубли: " + account.myRubles;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double rate = (double)edRate.Value;
+            double percent = (double)edPercent.Value;
+            account.sellAllDollars(rate, percent);
+            labelDollars.Text = "Мои доллары: " + account.myDollars;
+            labelRubles.Text = "Мои рубли: " + account.myRubles;
+        }
+    }
+
+    public class Wallet
+    {
+        public double myRubles = 1000;
+        public double myDollars = 0;
+        public void buyDollars(double val, double Rate, double percent)
+        {
+            double cost = val * Rate * (100+percent)/100;
+            if (myRubles >= cost)
+            {
+                myRubles -= cost;
+                myDollars += val;
+            }
+        }
+        public void sellDollars(double val, double Rate, double percent)
+        {
+            if (myDollars >= val)
+            {
+                myDollars -= val;
+                myRubles += val*Rate*(100-percent)/100;
+            }
+        }
+        public void buyAllDollars(double Rate, double percent)
+        {
+            double val = myRubles / Rate;
+            double cost = val * Rate * (100 + percent) / 100;
+            while (cost>myRubles && val>0)
+            {
+                val -= 0.01;
+                cost = val * Rate * (100 + percent) / 100;
+            }
+
+            if (val > 0) buyDollars(val, Rate, percent);
+        }
+        public void sellAllDollars(double Rate, double percent)
+        {
+            double val = myDollars;
+            sellDollars(val, Rate, percent);
+        }
+    }
+}
